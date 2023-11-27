16525b546ae66efa11c157ea792e9e519cd0b4ff
sebrahim <mohd.s.aqeel@gmail.com>
2023-11-27 13:40:06 +0300

    removing tracking attribute

--- models/content_plan.py
+++ models/content_plan.py
@@ -7,7 +7,7 @@ class ContentPlan(models.Model):
     active = fields.Boolean(default=True)
     plan_title = fields.Char(required=True)
     description = fields.Text()
-    partner_id = fields.Many2one('res.partner',string="Client",index= True,tracking=True,copy = False)
+    partner_id = fields.Many2one('res.partner',string="Client",index= True,copy = False)
     status = fields.Selection(
         string="Status",
         selection= [('draft', 'Draft'),('pending_approval', 'Pending Approval'),('modification', 'Modification'),('approved','Approved'),('canceled','Canceled')],
